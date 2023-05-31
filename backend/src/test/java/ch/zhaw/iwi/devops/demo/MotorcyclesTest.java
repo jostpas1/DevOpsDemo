a807e79f4ab0ccea5d4a90180ffa792edf3ef075
jostpas1 <32129279+jostpas1@users.noreply.github.com>
2023-05-31 20:00:54 +0200

    Testfall MotorcyclesTest.java

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MotorcyclesTest.java
@@ -0,0 +1,25 @@
+package ch.zhaw.iwi.devops.demo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class MotorcyclesTest {
+
+    @Test
+    void testMotorcyle() {
+        var motorcycle = new Motorcycles(42, "TestCycle", "TestType");
+        motorcycle.setId(42);
+        assertEquals("TestCycle", motorcycle.getTitle());
+        assertEquals("TestType", motorcycle.getMotorcycle_type());
+        assertEquals(42, motorcycle.getId());
+    }
+
+    @Test
+    void testEmptyBeer() {
+        var motorcycle = new Motorcycles();
+        motorcycle.setId(69);
+        assertEquals(null, motorcycle.getTitle());
+        assertEquals(null, motorcycle.getMotorcycle_type());
+        assertEquals(69, motorcycle.getId());
+    }
+    
+}
